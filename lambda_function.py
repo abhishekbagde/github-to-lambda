5db844f3866314ee465a808e83856829b7b8779b
Abhishek Antaram Bagde <abhishekbagde@in.ibm.com>
2021-09-20 23:25:23 +0530

    added csv file

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
+    d = "test.xlsx"
     df = pd.DataFrame(data=d)
     print(df)
     print('done x1.1')
